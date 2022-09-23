345c7d6b8bc148824d6362f645ab62bb9dbd46a1
Dishit79 <Dishit79@gmail.com>
2022-09-23 00:22:17 -0400

    Everything done

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ import { dirname, join } from "https://deno.land/x/opine/deps.ts";
 import { Database } from 'https://deno.land/x/aloedb/mod.ts';
 import { renderFile } from "https://deno.land/x/eta/mod.ts";
 
-
 const app = opine()
 const port = 5000
 const __dirname = dirname(import.meta.url);
@@ -18,6 +17,7 @@ interface StoreDB {
     points: number
     recentAddtion: string
     type: number
+    color: string
 }
 // Initialization
 const db = new Database<StoreDB>('points.json');
@@ -39,10 +39,34 @@ app.set('trust proxy', true)
 app.use(urlencoded());
 
 
+async function auth(req: any, res: any, next: any) {
+    const rawToken = req.get("cookie");
+    
+    try {
+        if (rawToken.substr(6) == "cG9rZW1vbmdvMjM0"){
+            next()
+    }
+    } catch{
+        res.redirect("/login")
+    }
+}
+
+
 app.post("/set/:year", async (req, res) => {
 
     let update = await setPoints(req.params.year, req.body.points)
     res.send(update)
+})
+
+app.post("/get/login", async (req, res) => {
+    const data = req.body 
+
+    if (data.password == "king100adminspassword" && data.username == "king100adminspassword"){
+        console.log("hiy");
+        res.cookie({ name: "token", value: "cG9rZW1vbmdvMjM0", expires: new Date(Date.now() + 600000), }).redirect("/admin")
+    } else {
+        res.redirect("/login")
+    }
 })
 
 app.get("/get/years", async (req, res) => {
@@ -50,6 +74,11 @@ app.get("/get/years", async (req, res) => {
     let update = await getManyYears()
     res.send(update)
 })
+
+app.get("/login", async (req, res) => {
+    res.render("login")
+})
+
 
 app.get("/", async (req, res) => {
     res.render("index")
@@ -60,7 +89,7 @@ app.get("/points", async (req, res) => {
     res.render("points", {points: points} )
 })
 
-app.get("/admin", async (req, res) => {
+app.get("/admin", await auth, async (req, res) => {
     const points = await getManyYears()
     res.render("test", { points: points })
 })
